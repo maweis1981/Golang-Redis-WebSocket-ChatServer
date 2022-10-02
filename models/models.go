f85ea9f9311087a47b6b4ac7085c6fc93f3cbea3
Maven <mawei.81@gmail.com>
2022-10-02 15:31:00 +0800

    merge legion & macbook pro

--- models/models.go
+++ models/models.go
@@ -14,7 +14,7 @@ type Msg struct {
 const (
 	CommandSubscribe = iota
 	CommandUnsubscribe
-	CommandChat
+	CommandCall
 )
 
 type MessagePayload struct {
@@ -22,7 +22,57 @@ type MessagePayload struct {
 }
 
 type SocketEventStruct struct {
-	EventName    string      `json:"event,omitempty"`
-	EventPayload interface{} `json:"payload,omitempty"`
-	SentTime     time.Time   `json:"sent_time,omitempty"`
+	EventName    string      `json:"event"`
+	EventPayload interface{} `json:"payload"`
+	SentTime     time.Time   `json:"sent_time"`
 }
+
+/**
+ON Signal Server
+Enroll - DeviceToken, Add to online-members-list
+TurnOn Personal Channel - DeviceToken As ChannelName
+TurnOff Personal Channel - DeviceToken As ChannelName
+SetStatus - DeviceToken, Status:IDLE(Default), BUSY, DoNotDisturb.
+CreateRoom - Room Master, Subscribe Room Channel
+Invite - Send Message to Callee
+Accept -
+
+On Client Side
+
+Call-Out - Generate Room ID and create Channel, Send Message to Callee,
+	Callee Get Message, Play Callee Audio/Video Ringtone.
+Call-In - Play Audio Ringtone, Video Ringtone, If Accept, the Channel subscriber will
+	be joined in the Audio/Video ChatRoom.
+
+Command:
+	Command
+
+struct {
+	DeviceToken
+	ChannelName
+	Command
+	Status
+	RoomID
+	RoomMaster
+	Caller
+	Callee
+	Content
+	Ringtone
+}
+
+URL : /ws/deviceToken
+enroll: {}
+TurnOn: {"command":0, deviceToken}
+TurnOff: {"command": 1, deviceToken}
+SetStatus: {"command": 3, deviceToken, "IDLE"}
+SetStatus: {"command": 3, deviceToken, "BUSY"}
+SetStatus: {"command": 3, deviceToken, "DoNotDisturb"}
+CreateRoom: {"command": 4, YouMeRoomId}
+Invite: {"command": 5, "callee_device_token", "youMeRoomId As Channel Name"}
+Accept: {"command": 6, ""}
+
+
+API: /api/users
+
+
+*/
